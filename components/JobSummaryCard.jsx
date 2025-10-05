ba78b3d172c25b3aba8849eff66c9342905834c8
Milton <ajaxmilton@hotmail.com>
2025-10-05 17:35:41 +0200

    Refactor JobSummaryCard by inlining JobSummaryCardButton component

--- components/JobSummaryCard.jsx
+++ components/JobSummaryCard.jsx
@@ -1,4 +1,12 @@
-import JobSummaryCardButton from "./JobSummaryCardButton";
+"use client";
+
+const JobSummaryCardButton = () => {
+  return (
+    <button className="bg-gray-950 text-white p-1 rounded-md hover:bg-gray-800 hover:cursor-pointer">
+      View Details
+    </button>
+  );
+};
 
 const JobSummaryCard = ({ job }) => {
   return (
