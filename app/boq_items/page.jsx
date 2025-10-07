882bc211228af9bbdd70815409604c124306e285
Milton <ajaxmilton@hotmail.com>
2025-10-08 01:02:30 +0200

    Improve from alignment

--- app/boq_items/page.jsx
+++ app/boq_items/page.jsx
@@ -23,7 +23,7 @@ export default function BoqItems() {
   }, [searchQuery]);
 
   return (
-    <main className="p-5">
+    <main className="p-5 max-w-6xl mx-auto">
       <h1 className="text-2xl font-bold mb-5">BOQ Items</h1>
       <div className="mb-5">
         <InputField
--- app/work_orders/page.jsx
+++ app/work_orders/page.jsx
@@ -2,12 +2,10 @@ import WorkOrderForm from "@/components/WorkOrderForm";
 
 export default function WorkOrdersPage() {
   return (
-    <main className="p-2 sm:p-5 flex flex-col items-center">
+    <main className="p-5 max-w-6xl mx-auto">
       <h1 className="text-2xl font-bold mb-5">Work Orders</h1>
-      <div className="w-full max-w-4xl">
-        <h2 className="text-xl font-bold mb-5">Add New Work Order</h2>
-        <WorkOrderForm />
-      </div>
+      <h2 className="text-xl font-bold mb-5">Add New Work Order</h2>
+      <WorkOrderForm />
     </main>
   );
 }
