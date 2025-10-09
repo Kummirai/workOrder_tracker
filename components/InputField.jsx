8ca4c978ca95943a01f491ac20661ab7a41ddda8
Milton <ajaxmilton@hotmail.com>
2025-10-09 19:15:30 +0200

    Fix input field text size for consistency

--- components/InputField.jsx
+++ components/InputField.jsx
@@ -14,7 +14,7 @@ const InputField = ({
         <span className="font-bold ml-1">{fieldLabel}</span>
         <input
           type={fieldtype}
-          className="border w-[100%] mt-1 rounded-s outline-blue-200 border-gray-200 p-1 text-[0.85rem]"
+          className="border w-[100%] mt-1 rounded-s outline-blue-200 border-gray-200 p-1 text-base"
           placeholder={placeholder}
           onChange={(e)=>handleChange(e)}
           value={inputValue}
