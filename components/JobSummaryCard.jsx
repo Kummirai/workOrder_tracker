42504e91f6051d3866f4318c3e6deeb5e1c6c8cd
Milton <ajaxmilton@hotmail.com>
2025-10-09 21:01:20 +0200

    Add react-icons dependency and update JobSummaryCard to use icon components for actions

--- components/JobSummaryCard.jsx
+++ components/JobSummaryCard.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { FaEye, FaPencilAlt, FaPrint, FaTrash } from 'react-icons/fa';
 
 const JobSummaryCard = ({ job, status, onDelete }) => {
   const getStatusColor = () => {
@@ -54,25 +55,16 @@ const JobSummaryCard = ({ job, status, onDelete }) => {
 
       <div className="flex justify-end gap-2 mt-4 pt-4 border-t border-gray-300">
         <Link href={`/work_orders/${job._id}`} className="p-2 rounded-full hover:bg-blue-100 text-blue-600 transition-colors duration-200" title="View">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-          </svg>
+          <FaEye className="w-5 h-5" />
         </Link>
         <Link href={`/work_orders/${job._id}/edit`} className="p-2 rounded-full hover:bg-yellow-100 text-yellow-600 transition-colors duration-200" title="Edit">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14.25v4.5a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 18.75v-11.25A2.25 2.25 0 015.25 5.25h4.5" />
-          </svg>
+          <FaPencilAlt className="w-5 h-5" />
         </Link>
         <Link href={`/work_orders/${job._id}/print`} target="_blank" rel="noopener noreferrer" className="p-2 rounded-full hover:bg-gray-100 text-gray-600 transition-colors duration-200" title="Print">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M6 16.5V7.5A2.25 2.25 0 018.25 5.25h7.5A2.25 2.25 0 0118 7.5v9M16.5 19.5h-9V16.5h9V19.5zM18 7.5H6v9h12V7.5z" />
-          </svg>
+          <FaPrint className="w-5 h-5" />
         </Link>
         <button onClick={handleDelete} className="p-2 rounded-full hover:bg-red-100 text-red-600 transition-colors duration-200 cursor-pointer" title="Delete">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.927a2.25 2.25 0 01-2.244-2.077L4.74 5.653m1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.927a2.25 2.25 0 01-2.244-2.077L4.74 5.653m1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.927a2.25 2.25 0 01-2.244-2.077L4.74 5.653M12 3.75h.007v.008H12V3.75zm-6.375.007H5.25V3.75h.375v.007zm13.5 0h.375V3.75h-.375v.007z" />
-          </svg>
+          <FaTrash className="w-5 h-5" />
         </button>
       </div>
     </div>
